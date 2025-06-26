35fd76fcceb1a756733b52267c7602624f3f4611
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-26 22:29:14 +0530

    redux-toolkit setup

---
+++ src/redux/appSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isMenuOpen: true,
+};
+
+const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    toggleMenu: (state) => {
+      state.isMenuOpen = !state.menuOpen;
+    },
+  },
+});
+
+export const { toggleMenu } = appSlice.actions;
+
+export default appSlice.reducer;
