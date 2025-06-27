bbaa7d6dc563c8934d6841fe96dc1dd85bb932ab
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-27 14:08:11 +0530

    react-routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import MainContainer from "./MainContainer";
 import Sidebar from "./sidebar";
-import VideoContainer from "./VideoContainer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const YOUTUBE_KEY = "AIzaSyC948PXj-2CAovO8omeOktgZ5f_4aQS8fo";
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_KEY;
