1bc239e6f3970d3ecd28dc97d58b93817a07ebb4
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-07-01 18:09:30 +0530

    fixing slice to remove old one

--- src/redux/chatSlice.js
+++ src/redux/chatSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { YOUTUBE_CHAT_COUNT } from "../utils/constants";
+
 const chatSlice = createSlice({
   name: "chat",
   initialState: {
@@ -7,8 +8,10 @@ const chatSlice = createSlice({
   },
   reducers: {
     addChat: (state, action) => {
-      state.messages.splice(YOUTUBE_CHAT_COUNT, 1);
-      state.messages.push(action.payload);
+      if (state.messages.length >= YOUTUBE_CHAT_COUNT) {
+        state.messages.shift(); // remove the first (oldest) item
+      }
+      state.messages.push(action.payload); // add the new message at the end
     },
   },
 });
